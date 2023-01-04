a3a09f103c4fc241bdc1118816027bd0e789d6fa
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:20:21 +0700

    add underscore sparator

--- Number.kt
+++ Number.kt
@@ -2,19 +2,19 @@ fun main() {
     //integer
     var age: Byte = 57
     var height: Int = 120
-    var distance: Short = 5000
-    var balance: Long = 5000000000L //L is sign for Long Data Type
+    var distance: Short = 5_000
+    var balance: Long = 5_000_000_000L //L is sign for Long Data Type
 
     //floating number
     var value: Float = 28.26F //F is sign for Float Data Type
-    var radius: Double = 38734834.87383832
+    var radius: Double = 38_734_834.87383832
 
     //literals
     var decimalLiteral: Int = 200 //Default
     var hexaLiteral: Int = 0xFA659C //0x is sign for hexadecimal number
     var binaryLiteral: Int = 0b00101010010
 
-    println(decimalLiteral)
-    println(hexaLiteral)//will print hexa number in decimal type (16410012)
-    println(binaryLiteral)//will print binary number in decimal type (338)
+    println(distance)
+    println(balance)//will print hexa number in decimal type (16410012)
+    println(radius)//will print binary number in decimal type (338)
 }
