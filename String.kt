eed5324896f3fe3820f2f9993186627eda14a05f
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:46:08 +0700

    manual concatenation

--- String.kt
+++ String.kt
@@ -3,15 +3,12 @@ import kotlin.concurrent.fixedRateTimer
 fun main() {
     var firstname: String = "Rohmat"
     var lastname: String = "Star"
-    var fullname: String = "Rohmat Star"
+    var fullname: String = firstname + " " + lastname
 
     var address: String = """
        -Jl. XYX No. 123
         -Banten - ID
     """.trimMargin(marginPrefix = "-")
 
-    println(firstname)
-    println(lastname)
     println(fullname)
-    println(address)
 }
