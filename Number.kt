4e138547e9fd54450bbf74d365868cda100fb7e2
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:13:09 +0700

    add floating number data type

--- Number.kt
+++ Number.kt
@@ -1,11 +1,14 @@
 fun main() {
+    //integer
     var age: Byte = 57
     var height: Int = 120
     var distance: Short = 5000
     var balance: Long = 5000000000L //L is sign for Long Data Type
 
-    println(age)
-    println(height)
-    println(distance)
-    println(balance)
+    //floating number
+    var value: Float = 28.26F //F is sign for Float Data Type
+    var radius: Double = 38734834.87383832
+
+    println(value)
+    println(radius)
 }
