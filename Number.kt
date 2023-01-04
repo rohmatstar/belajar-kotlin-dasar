41fa7302d9346daf6362b306fa857d0862c69acc
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:17:24 +0700

    add literals on number data type

--- Number.kt
+++ Number.kt
@@ -9,6 +9,12 @@ fun main() {
     var value: Float = 28.26F //F is sign for Float Data Type
     var radius: Double = 38734834.87383832
 
-    println(value)
-    println(radius)
+    //literals
+    var decimalLiteral: Int = 200 //Default
+    var hexaLiteral: Int = 0xFA659C //0x is sign for hexadecimal number
+    var binaryLiteral: Int = 0b00101010010
+
+    println(decimalLiteral)
+    println(hexaLiteral)//will print hexa number in decimal type (16410012)
+    println(binaryLiteral)//will print binary number in decimal type (338)
 }
