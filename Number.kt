506cc405ba067570a2df6ab4bb3e57d9d9015169
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:09:13 +0700

    number data type update

--- Number.kt
+++ Number.kt
@@ -1,5 +1,11 @@
 fun main() {
-    var age: Int = 57
+    var age: Byte = 57
+    var height: Int = 120
+    var distance: Short = 5000
+    var balance: Long = 5000000000L //L is sign for Long Data Type
 
     println(age)
+    println(height)
+    println(distance)
+    println(balance)
 }
