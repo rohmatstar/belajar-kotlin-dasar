f2b88df1de3f9c8269e0a414cc5d94c4107d1548
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:38:14 +0700

    string data type

---
+++ String.kt
@@ -0,0 +1,17 @@
+import kotlin.concurrent.fixedRateTimer
+
+fun main() {
+    var firstname: String = "Rohmat"
+    var lastname: String = "Star"
+    var fullname: String = "Rohmat Star"
+
+    var address: String = """
+       Jl. XYX No. 123
+        Banten - ID
+    """
+
+    println(firstname)
+    println(lastname)
+    println(fullname)
+    println(address)
+}
