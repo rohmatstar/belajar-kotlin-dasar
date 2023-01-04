e44a1f7ba629fdcdb3e2b52bbc43b0e57f8f15b3
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-01-04 16:41:13 +0700

    trimMargin() with marginPrefix

--- String.kt
+++ String.kt
@@ -6,9 +6,9 @@ fun main() {
     var fullname: String = "Rohmat Star"
 
     var address: String = """
-       |Jl. XYX No. 123
-        |Banten - ID
-    """.trimMargin()
+       -Jl. XYX No. 123
+        -Banten - ID
+    """.trimMargin(marginPrefix = "-")
 
     println(firstname)
     println(lastname)
